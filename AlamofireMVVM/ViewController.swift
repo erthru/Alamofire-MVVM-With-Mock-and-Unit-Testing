c51d1df7a00f5a15758737c4bae69da9f110935d
Suprianto D <ersaka96@gmail.com>
2019-07-11 15:57:27 +0800

    Initial Commit

---
+++ AlamofireMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireMVVM
+//
+//  Created by Suprianto Djamalu on 11/07/19.
+//  Copyright © 2019 Suprianto Djamalu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
